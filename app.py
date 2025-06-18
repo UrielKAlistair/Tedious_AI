224eb26db8b736e62b3b0585810bb9518344f847
Uriel K Alistair <arvindanuk@gmail.com>
2025-06-18 22:34:38 +0530

    vercel hosting added

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+@app.post("/api")
+async def api(query):
+    print(query)
+    return "Here is a dummy response to see if my endpoint works"
